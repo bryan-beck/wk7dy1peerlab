b8bbc9ee026a5109faf2c584b49b14b30730c76f
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2021-09-13 12:22:14 -0700

    troubleshooting

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
-const addName = require('./project');
-test('can add a new name',() => {
-  expect(addName('kyson')).toBe("Buenos dias kyson");
+const addUser = require('./project');
+test('can add a new user',() => {
+  expect(addUser('kyson')).toBe("Buenos dias kyson");
 });
