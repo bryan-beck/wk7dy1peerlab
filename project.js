e3fa6e2d9fa1691b3e548aaee3317ce9cc05fd66
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2021-09-13 12:13:41 -0700

    troubleshooting export issue

--- project.js
+++ project.js
@@ -2,9 +2,10 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
-module.exports = getGreeting;
+// module.exports = getGreeting;
 
 function createGreeting(f_Name){
     return "buenos dias" + f_Name;
 }
+module.exports = getGreeting;
 module.exports = createGreeting;
