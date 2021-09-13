6a2982f024ad689efd3aad217d2deee778b416b7
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2021-09-13 11:48:13 -0700

    opps

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
-// module.exports = getGreeting;
+module.exports = getGreeting;
 
 // function addName(fName){
 //     return "Adding " = fName;
